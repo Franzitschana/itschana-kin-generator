b5f47774fb9de8d07a910e5fe29a4952f17d7bd1
Franzitschana <zeitsurfer@gmail.com>
2025-06-21 10:07:27 +0200

    HomeText eingebunden – zentrale Botschaft sichtbar gemacht

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Layout from '../components/Layout';
+import Layout from './components/Layout';
+import HomeText from './components/HomeText';
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Itschana – KIN-Generator</h1>
-      <p>Heute ist KIN 216 – Weißer spektraler Weltenüberbrücker – Ton 11</p>
-      <p>Diese Seite wird gerade neu aufgebaut – mit Klarheit, Liebe und Struktur.</p>
+      <HomeText />
     </Layout>
   );
 }
