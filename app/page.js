f23ec9b3a0438d4f7e1e9fcc471b819a507eeffb
Franzitschana <zeitsurfer@gmail.com>
2025-06-21 15:05:25 +0200

    App-Router aktiviert – Startseite via /app/page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+import HomeText from '../components/HomeText';
+
+export default function Home() {
+  return (
+    <Layout>
+      <HomeText />
+    </Layout>
+  );
+}
