95cffa6688f2eb0a3bf87ff1947c4b3e886f423a
Franzitschana <zeitsurfer@gmail.com>
2025-06-21 10:50:36 +0200

    Archivseite angelegt – Raum für vergangene KIN-Tage geöffnet

---
+++ pages/archiv.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+export default function Archiv() {
+  return (
+    <Layout>
+      <h1>Archiv der KIN-Tage</h1>
+      <p>Hier entsteht ein Raum für die vergangenen KINs – sichtbar, lebendig, geordnet.</p>
+      <p>Demnächst kannst du hier alle Tage durchsuchen und entdecken.</p>
+    </Layout>
+  );
+}
