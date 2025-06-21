e295302efee8976953550a9819945c269ac8cd4d
Franzitschana <zeitsurfer@gmail.com>
2025-06-21 08:53:10 +0200

    Neuer Startpunkt gesetzt – klare Seite für Itschana geöffnett

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Willkommen im Itschana KIN-Generator</h1>
+      <p>Heute ist KIN 216 – Weißer spektraler Weltenüberbrücker – Ton 11</p>
+    </div>
+  );
+}
