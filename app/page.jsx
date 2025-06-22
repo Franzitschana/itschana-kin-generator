2dda52c08f0a132f1ba71f5d8283f03cff354377
Franzitschana <zeitsurfer@gmail.com>
2025-06-22 10:03:45 +0200

    Minimaltest: Nur h1 in Page

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,7 @@
-import Layout from '../components/Layout';
-import HomeText from '../components/HomeText';
-
 export default function Page() {
   return (
     <main>
-      <h1>Itschana – KIN-Generator</h1>
-      <p>Willkommen im Hüterraum der Zeit. Diese Seite zeigt dir, dass dein Projekt lebt ✨</p>
+      <h1>Itschana lebt ✨</h1>
     </main>
   );
 }
